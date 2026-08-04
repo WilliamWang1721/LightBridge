97e3f357b5499474dfc06a501680a8b5ee0b9b01
GeekMr <140129782+WilliamWang1721@users.noreply.github.com>
2026-08-04 14:18:50 +0800

    feat(ui): export Luma dropdown menu primitives

---
+++ frontend/src/components/ui/dropdown-menu/index.ts
@@ -0,0 +1,17 @@
+export {
+  DropdownMenuRoot as DropdownMenu,
+  DropdownMenuTrigger,
+  DropdownMenuGroup,
+  DropdownMenuSub,
+  DropdownMenuSubTrigger,
+  DropdownMenuSubContent,
+  DropdownMenuCheckboxItem,
+  DropdownMenuRadioGroup,
+  DropdownMenuRadioItem,
+  DropdownMenuItemIndicator,
+  DropdownMenuArrow,
+} from 'reka-ui'
+export { default as DropdownMenuContent } from './DropdownMenuContent.vue'
+export { default as DropdownMenuItem } from './DropdownMenuItem.vue'
+export { default as DropdownMenuLabel } from './DropdownMenuLabel.vue'
+export { default as DropdownMenuSeparator } from './DropdownMenuSeparator.vue'
