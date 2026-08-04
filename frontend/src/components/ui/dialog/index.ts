45f05b3df0b19bc595878ae20ed458f52a5c4194
GeekMr <140129782+WilliamWang1721@users.noreply.github.com>
2026-08-04 12:55:24 +0800

    feat(ui): export Luma dialog primitives

---
+++ frontend/src/components/ui/dialog/index.ts
@@ -0,0 +1,10 @@
+export {
+  DialogRoot as Dialog,
+  DialogTrigger,
+  DialogClose,
+  DialogTitle,
+  DialogDescription,
+} from 'reka-ui'
+export { default as DialogContent } from './DialogContent.vue'
+export { default as DialogHeader } from './DialogHeader.vue'
+export { default as DialogFooter } from './DialogFooter.vue'
