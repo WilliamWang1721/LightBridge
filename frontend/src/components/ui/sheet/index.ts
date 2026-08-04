541c545901e39303cb02a6c4f9ee66700525dfb4
GeekMr <140129782+WilliamWang1721@users.noreply.github.com>
2026-08-04 14:19:09 +0800

    feat(ui): export Luma sheet primitives

---
+++ frontend/src/components/ui/sheet/index.ts
@@ -0,0 +1,10 @@
+export {
+  DialogRoot as Sheet,
+  DialogTrigger as SheetTrigger,
+  DialogClose as SheetClose,
+  DialogTitle as SheetTitle,
+  DialogDescription as SheetDescription,
+} from 'reka-ui'
+export { default as SheetContent } from './SheetContent.vue'
+export { default as SheetHeader } from '../dialog/DialogHeader.vue'
+export { default as SheetFooter } from '../dialog/DialogFooter.vue'
